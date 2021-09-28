2515c8465558613f013d70af73f6be7a6f9bea71
Jacob_Vissering <jake.vissering@gmail.com>
2021-09-27 20:50:21 -0700

    Day4 Complete

--- Day4/day4exercies.py
+++ Day4/day4exercies.py
@@ -23,3 +23,42 @@ list_companies = companies.split(",")
 print(list_companies[1])
 
 print(company[10])
+
+python_acronym = "PFE"
+coding_acronym = "CFA"
+
+print(coding_acronym.find("C"))
+print(coding_acronym.find("F"))
+company="Coding for all People"
+print(company.rfind("l"))
+
+sentence = 'You cannot end a sentence with because because because is a conjunction'
+print(sentence.index("because"))
+print(sentence.rindex("because"))
+because_3 = sentence[31:54]
+print(because_3)
+print(sentence.find("because"))
+
+company="Coding For All"
+print(company.startswith("Coding"))
+print(company.endswith("coding"))
+company = "  Coding For All  "
+print(company.strip("  "))
+
+library_list = ["Django", "Flask", "Bottle", "Pyramid", "Falcon"]
+print("# ".join(library_list))
+
+print("I am enjoying this challenge. \nI just wonder what is next.")
+
+print("Name\tAge\tCountry\tCity \nAsabeneh\t250\tFindland\tHelsinki")
+
+
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+result = 'The area of a circle with radius {} is {}'.format(str(radius), str(area))
+print(result)
+
+print("""
+8 + 6 = 14
+8 - 6 =2""")
