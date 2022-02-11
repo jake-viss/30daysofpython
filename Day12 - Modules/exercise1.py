c8c4153dc0adf7f4c57955b000df8796db75e1d1
Jacob_Vissering <jake.vissering@gmail.com>
2022-02-11 09:12:16 -0800

    Update day 12 - E1

--- Day12 - Modules/exercise1.py
+++ Day12 - Modules/exercise1.py
@@ -1,8 +1,23 @@
 import random
 import string
-def random_user_id():
+
+# Write a function which generates a six digit/character random_user_id.
+'''def random_user_id():
     user_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range (6)])
     return user_id
 
-print(random_user_id())
+print(random_user_id())'''
 
+# Modify the previous task. Declare a function named user_id_gen_by_user. It doesn’t take any parameters but it takes two inputs using input(). One of the inputs is the number of characters and the second input is the number of IDs which are supposed to be generated.
+
+def user_id_gen_by_user():
+    num_characters = int(input("Input how many characters for User ID: "))
+    num_IDs = int(input("Input how many user IDs to create: "))
+    # list_Ids = []
+    for n in range(num_IDs):
+        user_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(num_characters)])
+        # list_Ids.append(user_id)
+        print(user_id)
+    # return list_Ids
+
+print(user_id_gen_by_user())
