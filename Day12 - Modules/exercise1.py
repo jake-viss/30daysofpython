32c15c0e509ec06460b81450ae193246ff84cb0b
Jacob_Vissering <jake.vissering@gmail.com>
2022-02-11 09:32:42 -0800

    Finish Exercise1

--- Day12 - Modules/exercise1.py
+++ Day12 - Modules/exercise1.py
@@ -21,3 +21,15 @@ def user_id_gen_by_user():
     # return list_Ids
 
 print(user_id_gen_by_user())
+
+# Write a function named rgb_color_gen. It will generate rgb colors (3 values ranging from 0 to 255 each).
+# rgb(125,244,255) - the output should be in this form
+def rgb_color_gen():
+    num1 = random.randint(0,255)
+    num2 = random.randint(0,255)
+    num3 = random.randint(0,255)
+    rgb = "rgb(" + str(num1)+","+str(num2)+","+str(num3)+")"
+    return rgb
+
+print(rgb_color_gen())
+
