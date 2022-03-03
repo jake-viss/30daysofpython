febe5a32fe29f0a0d22043520b751a32bf6db8b7
Jacob_Vissering <jake.vissering@gmail.com>
2022-03-02 16:33:47 -0800

    More work on e2 Day 14

--- Day14 - Higher Order Functions/exercise2.py
+++ Day14 - Higher Order Functions/exercise2.py
@@ -39,3 +39,38 @@ def six_exact(x):
 
 countries_not_six = filter(six_exact, countries)
 print(list(countries_not_six))
+
+# Use filter to filter out countries containing six letters and more in the country list.
+def six_or_more(x):
+    if len(x) >= 6:
+        return False
+    else:
+        return True
+
+countries_not_six = filter(six_or_more, countries)
+print(list(countries_not_six))
+
+# Use filter to filter out countries starting with an 'E'
+def out_E(x):
+    if x[0] != "E":
+        return True
+    else:
+        return False
+
+countries_no_e = filter(out_E, countries)
+print(list(countries_no_e))
+
+# Chain two or more list iterators (eg. arr.map(callback).filter(callback).reduce(callback))
+def uppercase(x):
+    return x.upper()
+
+def landless(x):
+    if "LAND" not in x:
+        return True
+    else:
+        return False
+
+
+
+upper_case_landless = filter(landless,(map(uppercase, countries)))
+print(list(upper_case_landless))
